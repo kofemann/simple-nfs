f708025f469aac6b9a3b306ccbbd86b3a8f58285
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2018-08-18 00:05:19 +0200

    handle command line options in App class

--- src/main/java/org/dcache/simplenfs/App.java
+++ src/main/java/org/dcache/simplenfs/App.java
@@ -1,31 +1,58 @@
 package org.dcache.simplenfs;
 
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.IOException;
+import java.nio.file.Path;
 import org.dcache.nfs.ExportFile;
 import org.dcache.oncrpc4j.portmap.OncRpcEmbeddedPortmap;
-import org.kohsuke.args4j.CmdLineParser;
 
 /**
  *
- * @author tigran
  */
 public class App {
 
+    @Option(name = "-root", usage = "root of the file system to export", metaVar = "<path>")
+    private Path root;
+    @Option(name = "-exports", usage = "path to file with export tables", metaVar = "<file>")
+    private Path exportsFile;
+    @Option(name = "-port", usage = "TCP port to use", metaVar = "<port>")
+    private int rpcPort = 2049;
+    @Option(name = "-with-portmap", usage = "start embedded portmap")
+    private boolean withPortmap;
+
     public static void main(String[] args) throws Exception {
+        new App().run(args);
+    }
 
-        Arguments arguments = new Arguments();
-        CmdLineParser parser = new CmdLineParser(arguments);
-        parser.parseArgument(args);
+    public void run(String[] args) throws CmdLineException, IOException {
 
-        ExportFile exportFile = null;
-        if (arguments.getExportsFile()!=null) {
-            exportFile = new ExportFile(arguments.getExportsFile().toFile());
+        CmdLineParser parser = new CmdLineParser(this);
+
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println();
+            System.err.println(e.getMessage());
+            System.err.println("Usage:");
+            System.err.println("    App [options...]");
+            System.err.println();
+            parser.printUsage(System.err);
+            System.exit(1);
         }
 
-        if (arguments.getWithPortmap()) {
+        ExportFile exportFile = null;
+        if (exportsFile != null) {
+            exportFile = new ExportFile(exportsFile.toFile());
+        }
+
+        if (withPortmap) {
             new OncRpcEmbeddedPortmap();
         }
 
-        try (SimpleNfsServer ignored = new SimpleNfsServer(arguments.getRpcPort(), arguments.getRoot(), exportFile, null)) {
+        try (SimpleNfsServer ignored = new SimpleNfsServer(rpcPort, root, exportFile, null)) {
             //noinspection ResultOfMethodCallIgnored
             System.in.read(); //any key to shutdown
         }
